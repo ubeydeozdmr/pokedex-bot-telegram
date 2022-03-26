710ffb67df7baabb6a8c39b1367350f8fe43edd2
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2022-03-26 15:20:01 +0300

    Deployment to Heroku

--- index.js
+++ index.js
@@ -16,13 +16,3 @@ const pokemonListCommand = require('./src/commands/pokemonList');
 pokemonListCommand(bot);
 
 bot.launch();
-
-// exports.handler = (event, context, callback) => {
-//   const tmp = JSON.parse(event.body); // get data passed to us
-//   bot.handleUpdate(tmp); // make Telegraf process that data
-//   return callback(null, {
-//     // return something for webhook, so it doesn't try to send same stuff again
-//     statusCode: 200,
-//     body: '',
-//   });
-// };
