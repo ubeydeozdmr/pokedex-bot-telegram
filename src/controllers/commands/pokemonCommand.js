6296c9179f88bbd4b7f449ea7b20fe58c54c5b6a
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2022-03-31 00:42:17 +0300

    Bugs fixed

--- src/controllers/commands/pokemonCommand.js
+++ src/controllers/commands/pokemonCommand.js
@@ -1,4 +1,8 @@
-const { REJECT_INVALID_INPUT } = require('../../config');
+const {
+  REJECT_INVALID_INPUT,
+  NOT_FOUND_PKNAME,
+  NOT_FOUND_PKID,
+} = require('../../config');
 const getPokemonModel = require('../../models/pokemonModel');
 const getPokemonView = require('../../views/pokemonView');
 
@@ -13,7 +17,15 @@ module.exports = bot => {
       input.push(temp);
     }
 
+    if (isFinite(input)) {
+      if (input > 10000) {
+        return ctx.reply(NOT_FOUND_PKID);
+      }
+    }
+
     const pokemonData = await getPokemonModel(ctx, input[0]);
+    if (pokemonData && pokemonData[2] > 10000)
+      return ctx.reply(NOT_FOUND_PKNAME);
     if (pokemonData) getPokemonView(bot, ctx, ...pokemonData);
   });
 };
